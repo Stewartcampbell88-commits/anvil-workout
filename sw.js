64a154ce2ca4114bd9990f4e950c09b3ddbee045
Stewartcampbell88-commits <Stewartcampbell88-commits@users.noreply.github.com>
2026-09-23 11:04:53 +1000

    Switch ANVIL to a 2-day A/B whole-body split for phone preview.
    Replace the single Session with Day A (squat/bench/row + accessories + farmer) and Day B (RDL/OHP/BSS + accessories + heaves); bump SW cache to anvil-v5.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "anvil-v4";
+const CACHE = "anvil-v5";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL).catch(() => c.addAll(["./", "./index.html"]))));
