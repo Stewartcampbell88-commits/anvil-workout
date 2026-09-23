5c95d17e353d5b43fe592936b7cd89ae30426fb4
Stewartcampbell88-commits <Stewartcampbell88-commits@users.noreply.github.com>
2026-09-23 16:42:55 +1000

    Aussify rude and adult rest cues
    Replace the old cue phrases with natural Australian gym banter and bump the service worker cache for Pages clients.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "anvil-v10";
+const CACHE = "anvil-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL).catch(() => c.addAll(["./", "./index.html"]))));
